45873e4d05c3443d75597d9aa57d091b7d90f548
solpark-dev <solpark-dev@gmail.com>
2026-03-25 00:13:31 +0900

    chore: remove sensitive files, temp scripts, and redundant gitignores

--- 4beans-moa-front/_w.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const c = fs.readFileSync("src/pages/landing/LandingPageYSa01.jsx", "utf8");
-const heroMatch = c.match(/const ParallaxHero[\s\S]*?^};/m);
-console.log("Script created");
